ab92b2f2397dda89988c80aa7fdd4c242fec058e
5710110015 <5710110015@psu.ac.th>
2019-02-27 15:09:15 +0700

    edit Gameplay

--- GameSnake/GamePlay.java
+++ GameSnake/GamePlay.java
@@ -1,6 +1,9 @@
 public class GamePlay{
     public static void main(String args[]){
         GameSnake g = new GameSnake(30);
-        g.play();
+
+        while(!g.isEnded()){
+            g.play();
+        }
     }
 }
